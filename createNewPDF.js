246b6f32e545c111b10f6cd148e252ce9f9aee4b
Girish_Dhanawade <109794360+GirishDhanawade@users.noreply.github.com>
2023-05-18 10:36:30 +0530

    Add files via upload

--- createNewPDF.js
+++ createNewPDF.js
@@ -1,74 +1,97 @@
-import { LightningElement, track, wire,api } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
-import { getRecord } from 'lightning/uiRecordApi';
-import { CurrentPageReference } from 'lightning/navigation';
+import { getRecord, createRecord } from 'lightning/uiRecordApi';
+import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import downloadjs from '@salesforce/resourceUrl/downloadjs';
-import downloadPDF from '@salesforce/apex/PrintJobPDFController.getPDFprint';
-import sharePdf from '@salesforce/apex/PrintJobPDFController.sharePdf';
 import USER_EMAIL_FIELD from '@salesforce/schema/User.Email';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import generatePDFContent from '@salesforce/apex/PrintJobPDF.generateBase64PDFContent';
+import sharePdf from '@salesforce/apex/PrintJobPDF.sharePdf';
 import { CloseActionScreenEvent } from 'lightning/actions';
 
 const USER_FIELDS = [USER_EMAIL_FIELD];
-export default class CreateNewPDF extends LightningElement {
-    @track showButtons = true;
-    @track pdfUrl;
-    @track boolShowSpinner = false;
-    userEmail;
-    recordId;
-    async connectedCallback(){
-       await this.generatePdf();
-       await this.sharePDF();
+
+export default class CreateNewPDF extends NavigationMixin(LightningElement) {
+  @track showButtons = true;
+  @track pdfUrl;
+  @track boolShowSpinner = false;
+  userEmail;
+  recordId;
+
+  connectedCallback() {
+    this.generatePdf();
+  }
+
+  @wire(CurrentPageReference)
+  currentPageReferenceHandler(currentPageReference) {
+    if (currentPageReference && currentPageReference.state) {
+      this.recordId = currentPageReference.state.recordId;
     }
-    @wire(CurrentPageReference)
-    currentPageReferenceHandler(currentPageReference) {
-        if (currentPageReference && currentPageReference.state) {
-            this.recordId = currentPageReference.state.recordId;
+  }
+
+  @wire(CurrentPageReference) pageRef;
+  @wire(getRecord, { recordId: '$accountId', fields: USER_FIELDS })
+  wireAccount({ error, data }) {
+    if (data) {
+      this.userEmail = data.fields.Email.value;
+    } else if (error) {
+      console.error('Error retrieving account information:', error);
+    }
+  }
+
+  renderedCallback() {
+    loadScript(this, downloadjs)
+      .then(() => console.log('Loaded download.js'))
+      .catch(error => console.log(error));
+  }
+
+  async generatePdf() {
+    this.showButtons = false;
+    this.boolShowSpinner = true;
+    try {
+      const result = await generatePDFContent();
+      console.log('Result = ' + result);
+      if (result) {
+        const contentDocumentRecord = {
+          apiName: 'ContentVersion',
+          fields: {
+            Title: 'attachment.pdf',
+            PathOnClient: 'attachment.pdf',
+            VersionData: result,
+            FirstPublishLocationId: this.recordId
+          }
+        };
+        console.log('ContentDocumentRecord = ' + contentDocumentRecord);
+        console.log('Result123 = ' + result);
+        this.dispatchEvent(new CloseActionScreenEvent());
+        const createdRecord = await createRecord(contentDocumentRecord);
+        console.log('CreateRecord = ' + createdRecord);
+        console.log('RecordId = ' + createdRecord.id);
+        if (createdRecord.id) {
+          this.pdfUrl =
+            '/sfc/servlet.shepherd/document/download/'+createdRecord.id;
+            console.log('Final log'+ this.pdfUrl);
+           // this.sharePDF();
+          window.open(this.pdfUrl);
         }
+      }
+    } catch (error) {
+      console.log('Error:', error.body ? error.body.message : error);
     }
-    @wire(CurrentPageReference) pageRef;
-    @wire(getRecord, { recordId: '$accountId', fields: USER_FIELDS })
-    wireAccount({ error, data }) {
-        if (data) {
-            this.userEmail = data.fields.Email.value;
-        } else if (error) {
-            console.error('Error retrieving account information:', error);
-        }
-    }
-    renderedCallback() {
-        loadScript(this, downloadjs)
-            .then(() => console.log('Loaded download.js'))
-            .catch(error => console.log(error));
-    }
-    get accountId() {
-        return this.pageRef && this.pageRef.state && this.pageRef.state.recordId;
-    }
-    async generatePdf() {
-        this.showButtons = false;
-        this.boolShowSpinner = true;
-        downloadPDF({})
+  }
+
+  async sharePDF() {
+    this.showButtons = false;
+    if (this.recordId) {
+        console.log('RecordId = ' + this.recordId);
+        sharePdf({ recordId: this.recordId })
             .then(response => {
-                console.log('response[0]====>' + response[0]);
-                this.pdfUrl = response[1];
-               //window.location.href = this.pdfUrl; for same page
-                window.open(this.pdfUrl);//for new page
-                this.dispatchEvent(new CloseActionScreenEvent());
+                console.log('Success: ' + response);
             })
             .catch(error => {
-                console.log('Error: ' + error.body.message);
-            })
+                console.error('Error: ' + error.body.message);
+            });
     }
-
-    async sharePDF() {
-        this.showButtons = false;
-        if (this.recordId) {
-            console.log('RecordId = ' + this.recordId);
-            sharePdf({ recordId: this.recordId })
-                .then(response => {
-                    console.log('Success: ' + response);
-                })
-                .catch(error => {
-                    console.error('Error: ' + error.body.message);
-                });
-        }
-    }  
+}  
 }
